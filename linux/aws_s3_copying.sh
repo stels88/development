08648dcc79b2b8b98e637cbf3a9251ec90d9bc28
Oleg Smirnov <oleg.smirnov.1988@gmail.com>
2023-06-15 16:40:28 +0300

    Update aws_s3_copying.sh

--- linux/aws_s3_copying.sh
+++ linux/aws_s3_copying.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#
+# AWS S3 buckets backup
+# Oleg Smirnov <oleg.smirnov.1988@gmail.com>
+#  
+export AWS_ACCESS_KEY_ID=AKIA2CLNEANBLHKRDM2F
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=eu-central-1
 
+aws_s3_buckets=`aws s3 ls | awk {'print $3'}`
+mkdir -pv $AWS_DEFAULT_REGION && cd $AWS_DEFAULT_REGION
+
+for aws_s3_bucket in $aws_s3_buckets; do
+    mkdir -pv $aws_s3_bucket
+    aws s3 sync s3://$aws_s3_bucket $aws_s3_bucket
+done
