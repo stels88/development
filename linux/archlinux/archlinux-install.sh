8deaf784546d71467f7c1660e7e88a5decdb29ec
Oleg Smirnov <oleg.smirnov.1988@gmail.com>
2023-06-16 14:25:30 +0300

    Update archlinux-install.sh

--- linux/archlinux/archlinux-install.sh
+++ linux/archlinux/archlinux-install.sh
@@ -4,6 +4,14 @@
 #
 
 
+# fdisk /dev/the_disk_to_be_partitioned
+# mkfs.ext4 /dev/root_partition
+# mkswap /dev/swap_partition
+
+fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+
 useradd -m stealth
 passwd stealth
 
