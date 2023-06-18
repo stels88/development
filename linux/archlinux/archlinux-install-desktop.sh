f58f83585ef09d0363a86104b62b60ee94ecf1f3
Oleg Smirnov <oleg.smirnov.1988@gmail.com>
2023-06-18 13:21:53 +0300

    Update archlinux-install-desktop.sh

--- linux/archlinux/archlinux-install-desktop.sh
+++ linux/archlinux/archlinux-install-desktop.sh
@@ -1,1 +1,4 @@
-
+# vlc
+# Packages (16) ffmpeg4.4-4.4.3-2  glslang-12.1.0-2  libdvbpsi-1:1.3.3-2  libebml-1.4.4-1  libmatroska-1.7.1-1  libplacebo-5.229.2-2  libtar-1.2.20-6  libupnp-1.14.17-1
+#               lua52-5.2.4-5  qt5-base-5.15.9+kde+r151-1  qt5-svg-5.15.9+kde+r8-1  qt5-translations-5.15.9-1  qt5-x11extras-5.15.9-1  shaderc-2023.3-1  spirv-tools-2022.4-1
+#               vlc-3.0.18-9
