b5aed56c6e5bb5e7e952bcfbaf72cfb9d8b6f873
Oleg Smirnov <oleg.smirnov.1988@gmail.com>
2025-10-01 22:11:20 +0300

    Create archlinux-install-extra-audio.sh

---
+++ linux/dell/m6800/archlinux-install-extra-audio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Oleg Smirnov <oleg.smirnov.1988@gmail.com>
+# Dell M6800
+
+# Sound Open Firmware
+# URL : https://archlinux.org/packages/extra/x86_64/sof-firmware/
+pacman -S sof-firmware
+
+# PulseAudio Volume Control
+# URL : https://archlinux.org/packages/extra/x86_64/pavucontrol/
+pacman -S pavucontrol
